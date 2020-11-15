a15fa382e36f05d37faacaca67da054b89f812b5
Mark Szulc <mszulc@adobe.com>
2020-11-15 20:06:49 +1100

    Update watcher.py

--- watcher.py
+++ watcher.py
@@ -123,12 +123,6 @@ elif jsonresult['status'] == "running":
 			
 
 			
-uh.set_layout(uh.PHAT)
-uh.brightness(0.5)
-uh.set_pixel(0, 0, 255, 0, 0)
-uh.show()
-
-time.sleep(1)
 
 uh.set_pixel(0, 0, 0, 255, 0)
 uh.show()
