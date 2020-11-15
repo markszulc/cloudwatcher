f6ed42e01b5af0037502b780fc60077d9de67223
Mark Szulc <mszulc@adobe.com>
2020-11-15 18:20:33 +1100

    Fixed Lightpoint

--- watcher.py
+++ watcher.py
@@ -103,6 +103,66 @@ def switchBlue() :
 	blinkThread.start()
 	
 	
+def switchOff() :
+	global blinkThread, globalBlue, globalGreen, globalRed
+	globalRed = 0
+	globalGreen = 0
+	globalBlue = 0
+	if blinkThread != None :
+		blinkThread.do_run = False
+	unicorn.clear()
+	unicorn.off()
+
+class LightPoint:
+
+	def __init__(self):
+		self.direction = randint(1, 4)
+		if self.direction == 1:
+			self.x = randint(0, width - 1)
+			self.y = 0
+		elif self.direction == 2:
+			self.x = 0
+			self.y = randint(0, height - 1)
+		elif self.direction == 3:
+			self.x = randint(0, width - 1)
+			self.y = height - 1
+		else:
+			self.x = width - 1
+			self.y = randint(0, height - 1)
+
+		self.colour = []
+		for i in range(0, 3):
+			self.colour.append(randint(100, 255))
+
+
+def update_positions():
+
+	for point in points:
+		if point.direction == 1:
+			point.y += 1
+			if point.y > height - 1:
+				points.remove(point)
+		elif point.direction == 2:
+			point.x += 1
+			if point.x > width - 1:
+				points.remove(point)
+		elif point.direction == 3:
+			point.y -= 1
+			if point.y < 0:
+				points.remove(point)
+		else:
+			point.x -= 1
+			if point.x < 0:
+				points.remove(point)
+
+
+def plot_points():
+
+	unicorn.clear()
+	for point in points:
+		unicorn.set_pixel(point.x, point.y, point.colour[0], point.colour[1], point.colour[2])
+	unicorn.show()
+
 def blinkRandom(arg):
 	t = threading.currentThread()
 	while getattr(t, "do_run", True):
